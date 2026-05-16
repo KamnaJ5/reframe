4b1723f7d8d629ae26a518e9b14d9174d9883c22
Rave <thekamna5j@gmail.com>
2026-05-16 18:11:26 +0530

    Fix formatting issues in VideoEditor component

--- src/components/VideoEditor.tsx
+++ src/components/VideoEditor.tsx
@@ -237,7 +237,6 @@ export default function VideoEditor() {
     </div>
 
   </div>
-</Section>
                     <AudioSpeedControl recipe={recipe} onChange={updateRecipe} />
                   <Section icon={<SlidersHorizontal size={12} />} title="Output format" delay={190}>
                     <FormatSelector recipe={recipe} onChange={updateRecipe} />
